df83b7044a69904d5991467707ffcec4e37290ab
Yevhen <yevhenbekir@gmail.com>
2023-04-30 01:51:35 +0300

    more than better

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-const express = require("express");
 const jsonServer = require("json-server");
-const path = require("path");
-
-const server = express();
-
-const apiServer = jsonServer.create();
-const apiRouter = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
-apiServer.use(middlewares);
-apiServer.use(apiRouter);
-apiServer.listen(process.env.PORT || 3001);
-
-server.use(express.static(path.join(__dirname, "build")));
-
-server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-server.listen(process.env.PORT || 3000);
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
