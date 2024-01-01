175b495935b2bde01a446378521f520f24828e28
Yevhen Bekir <yevhenbekir@gmail.com>
2024-01-01 21:42:27 +0200

    update dependencies

--- src/services/useCompaniesRequest.js
+++ src/services/useCompaniesRequest.js
@@ -4,7 +4,7 @@ const useCompaniesRequest = () => {
   const { request } = useHttp();
 
   const getCompanies = async (city) => {
-    const response = await request(`http://168.119.50.224:3001/cityCompanies`);
+    const response = await request(`http://localhost:3001/cityCompanies`);
     return transformCompanies(response, city);
   };
 
